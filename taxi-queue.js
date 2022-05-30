c55a8be952fa49b0452256e092ebde9cf3c8673a
Anthony <almogotlane@gmail.com>
2022-05-30 12:03:47 +0200

    Added logic to the factory function to pass the test

--- taxi-queue.js
+++ taxi-queue.js
@@ -1,28 +1,37 @@
-function TaxiQueue() {
+function TaxiQueue(queue, taxiQueue) {
+	let theQueue = queue ? queue : 0;
+	let theTaxiQueue = taxiQueue ? taxiQueue : 0;
 
 
 	function joinQueue() {
-
+		theQueue++;
 	}
 
 	function leaveQueue() {
-
+		if(theQueue > 0) {
+			theQueue--;
+		}
 	}
 
 	function joinTaxiQueue() {
-
+		theTaxiQueue++;
 	}
 
 	function queueLength() {
-
+		return theQueue;
 	}
 
 	function taxiQueueLength() {
-
+		return theTaxiQueue;
 	}
 
 	function taxiDepart(){
-
+		if(theQueue >= 12) {
+			if(theTaxiQueue > 0) {
+				theQueue -= 12;
+				theTaxiQueue -= 1;
+			}
+		}
 	}
 
 	return {
