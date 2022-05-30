346d90f3eb707b6d310b5364c0a98cf859fc973e
Anthony <almogotlane@gmail.com>
2022-05-30 14:32:44 +0200

    Added logic to depart button and localStorage

--- js/taxi-queue.dom.js
+++ js/taxi-queue.dom.js
@@ -2,29 +2,42 @@
 let joinTheQueue = document.querySelector(".join_queue");
 let leaveTheQueue = document.querySelector(".leave_queue");
 let joinTheTaxiQueue = document.querySelector(".join_taxi_queue");
-let taxiDepart = document.querySelector(".depart");
+let depart = document.querySelector(".depart");
 
 // create Factory Function instance
-const taxiQueue = TaxiQueue();
+//initiate values from local storage
+const taxiQueue = TaxiQueue(JSON.parse(localStorage.getItem("people-queue")), JSON.parse(localStorage.getItem("taxi-queue")));
 
-// write your DOM code here.
+//write your DOM code here.
 // DOM events
 //add to queue
 joinTheQueue.addEventListener("click", () => {
     taxiQueue.joinQueue();
     counterTemplate(taxiQueue.queueLength(), ".passenger_content", ".passenger_container");
+    localStorage.setItem("people-queue", JSON.stringify(taxiQueue.queueLength()));
 })
 //remove from queue
 leaveTheQueue.addEventListener("click", () => {
     taxiQueue.leaveQueue();
     counterTemplate(taxiQueue.queueLength(), ".passenger_content", ".passenger_container");
+    localStorage.setItem("people-queue", JSON.stringify(taxiQueue.queueLength()))
 })
 //add when a taxi joins the queue
 joinTheTaxiQueue.addEventListener("click", () => {
     taxiQueue.joinTaxiQueue();
     counterTemplate(taxiQueue.taxiQueueLength(), ".taxi_content", ".taxi_container");
+    localStorage.setItem("taxi-queue", JSON.stringify(taxiQueue.taxiQueueLength()));
 })
+//depart event button
+depart.addEventListener("click", () => {
+    taxiQueue.taxiDepart();
 
+    counterTemplate(taxiQueue.queueLength(), ".passenger_content", ".passenger_container");
+    counterTemplate(taxiQueue.taxiQueueLength(), ".taxi_content", ".taxi_container");
+
+    localStorage.setItem("taxi-queue", JSON.stringify(taxiQueue.taxiQueueLength()));
+    localStorage.setItem("people-queue", JSON.stringify(taxiQueue.queueLength()));
+})
 
 //handlebars counters templates function
 function counterTemplate(theCount, contentClass, containerClass) {
@@ -33,7 +46,7 @@ function counterTemplate(theCount, contentClass, containerClass) {
     let counterObj = {
         counter: theCount
     }
-    document.querySelector(containerClass).innerHTML = counterTemplateFunc(counterObj)
+    document.querySelector(containerClass).innerHTML = counterTemplateFunc(counterObj);
 }
 counterTemplate(taxiQueue.queueLength(), ".passenger_content", ".passenger_container");
 counterTemplate(taxiQueue.taxiQueueLength(), ".taxi_content", ".taxi_container");
